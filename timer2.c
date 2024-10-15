ff06ce033dd3f8ef89008b3f0bed346271e1db8f
Jevi Waugh <codeprogramming99@gmail.com>
2024-10-15 17:54:26 +1000

    joystick setup

--- timer2.c
+++ timer2.c
@@ -16,6 +16,10 @@ volatile uint8_t number_to_display; // Default to 42 for testing
 
 // Track which digit to display (0 = right, 1 = left)
 volatile uint8_t current_digit = 0;
+volatile uint8_t x_or_y = 0;
+
+volatile uint16_t joy_x = 0;
+volatile uint16_t joy_y = 0;
 
 void init_timer2(void)
 {
@@ -27,11 +31,11 @@ void init_timer2(void)
 	** resets to 0.
 	** We divide the clock by 8 and count 1000 cycles (0 to 999)
 	*/
-	OCR2A = 999;
+	OCR2A = 249;
 	TCCR2A = (0 << COM2A1) | (1 << COM2A0)  // Toggle OC2A on compare match
 		| (0 << WGM21) | (0 << WGM20); // Least two significant WGM bits
 	TCCR2B = (1 << WGM22) // Two most significant WGM bits
-		| (0 << CS22) | (1 << CS21) | (0 <<CS20); // Divide clock by 8
+		| (0 << CS22) | (1 << CS21) | (1 <<CS20); // Divide clock by 8
 
 	/* Enable interrupt on timer on output compare match 
 	*/
@@ -67,4 +71,50 @@ ISR(TIMER2_COMPA_vect) {
 
     // Switch to the other digit for the next interrupt
     current_digit = 1 - current_digit;
+
+	uint16_t value_2;
+	
+	
+	/* Set up the serial port for stdin communication at 19200 baud, no echo */
+	// init_serial_stdio(19200,0);
+	
+	// /* Turn on global interrupts */
+	// sei();
+	
+	// Set up ADC - AVCC reference, right adjust
+	// Input selection doesn't matter yet - we'll swap this around in the while
+	// // loop below.
+	ADMUX = (1<<REFS0);
+	// // Turn on the ADC (but don't start a conversion yet). Choose a clock
+	// // divider of 64. (The ADC clock must be somewhere
+	// // between 50kHz and 200kHz. We will divide our 8MHz clock by 64
+	// // to give us 125kHz.)
+	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1);
+	
+	// /* Print a welcome message
+	// */
+	// printf("ADC Test\n");
+	
+	
+	// Set the ADC mux to choose ADC0 if x_or_y is 0, ADC1 if x_or_y is 1
+	if(x_or_y == 0) {
+		ADMUX &= ~1;
+	} else {
+		ADMUX |= 1;
+	}
+	// Start the ADC conversion
+	ADCSRA |= (1<<ADSC);
+	
+	
+	value_2 = ADC; // read the value
+	if(x_or_y == 0) {
+		// printf("X: %4d ", value_2);
+		joy_x = value_2;
+	} else {
+		// printf("Y: %4d\n", value_2);
+		joy_y = value_2;
+	}
+	// // Next time through the loop, do the other direction
+	x_or_y ^= 1;
+	
 }
--- timer2.h
+++ timer2.h
@@ -11,7 +11,8 @@
 #include <stdint.h>
 
 extern volatile uint8_t number_to_display; // Default to 42 for testing
-
+extern volatile uint16_t joy_x;
+extern volatile uint16_t joy_y;
 /// <summary>
 /// Skeletal timer 2 initialisation function.
 /// </summary>
