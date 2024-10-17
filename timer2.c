db0c31cda8a49566aa00f7245f193b193cab0666
Jevi Waugh <codeprogramming99@gmail.com>
2024-10-17 14:45:16 +1000

    removed joystick definition

--- timer2.c
+++ timer2.c
@@ -81,40 +81,6 @@ ISR(TIMER2_COMPA_vect) {
 	// /* Turn on global interrupts */
 	// sei();
 	
-	// Set up ADC - AVCC reference, right adjust
-	// Input selection doesn't matter yet - we'll swap this around in the while
-	// // loop below.
-	ADMUX = (1<<REFS0);
-	// // Turn on the ADC (but don't start a conversion yet). Choose a clock
-	// // divider of 64. (The ADC clock must be somewhere
-	// // between 50kHz and 200kHz. We will divide our 8MHz clock by 64
-	// // to give us 125kHz.)
-	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1);
 	
-	// /* Print a welcome message
-	// */
-	// printf("ADC Test\n");
-	
-	
-	// Set the ADC mux to choose ADC0 if x_or_y is 0, ADC1 if x_or_y is 1
-	if(x_or_y == 0) {
-		ADMUX &= ~1;
-	} else {
-		ADMUX |= 1;
-	}
-	// Start the ADC conversion
-	ADCSRA |= (1<<ADSC);
-	
-	
-	value_2 = ADC; // read the value
-	if(x_or_y == 0) {
-		// printf("X: %4d ", value_2);
-		joy_x = value_2;
-	} else {
-		// printf("Y: %4d\n", value_2);
-		joy_y = value_2;
-	}
-	// // Next time through the loop, do the other direction
-	x_or_y ^= 1;
 	
 }
